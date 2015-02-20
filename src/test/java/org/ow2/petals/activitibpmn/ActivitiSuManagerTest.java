petalslink/petals-se-flowable

commit 9839ac514a1ebf29a221ccdb80f460985e328ca9

Improve unit tests of the service unit manager

src/main/java/org/ow2/petals/activitibpmn/ActivitiSuManager.java
@@ -269,6 +269,8 @@ public class ActivitiSuManager extends AbstractServiceUnitManager {
                     bpmnModels.put(processFileName,
                             this.readBpmnModel(processFileName, versionStr, tenantId, categoryId, suRootPath));
                     nbProcesses++;
+                } else {
+                    throw new IncoherentProcessDefinitionDeclarationException(processFileName, versionStr);
                 }
             } while (processFileName != null && versionStr != null);
         } else if (processFileName != null && versionStr != null) {

src/main/java/org/ow2/petals/activitibpmn/ActivitiSEConstants.java
@@ -27,6 +27,11 @@ import javax.xml.namespace.QName;
 public final class ActivitiSEConstants {
 
     /**
+     * Name space name of service unit parameters
+     */
+    public static final String NAMESPACE_SU = "http://petals.ow2.org/components/petals-se-activitibpmn/su/1.0";
+
+    /**
      * The SU parameter that defines the tenantId.
      */
     public static final String TENANT_ID = "tenant_id";

commit 7ad237f99bacb577f9a6026a594644e37080bb7b

Fix a regression introduced by commit 9839ac5: unable to deploy a valid service unit containing multiple process definitions

samples/su-activitibpmn-vacationRequest-provide/src/main/jbi/jbi.xml
@@ -38,10 +38,10 @@
 			<!-- ACTIVITI specific fields -->
  			<petals-se-activitibpmn:tenant_id>BEStenant</petals-se-activitibpmn:tenant_id>
 			<petals-se-activitibpmn:category_id>TEST</petals-se-activitibpmn:category_id>
-			<petals-se-activitibpmn:process_file>vacationRequest.bpmn20.xml</petals-se-activitibpmn:process_file>
-			<petals-se-activitibpmn:version>1</petals-se-activitibpmn:version>
-			<petals-se-activitibpmn:process_file>orderProcess.bpmn20.xml</petals-se-activitibpmn:process_file>
- 			<petals-se-activitibpmn:version>1</petals-se-activitibpmn:version>
+			<petals-se-activitibpmn:process_file1>vacationRequest.bpmn20.xml</petals-se-activitibpmn:process_file1>
+			<petals-se-activitibpmn:version1>1</petals-se-activitibpmn:version1>
+			<petals-se-activitibpmn:process_file2>orderProcess.bpmn20.xml</petals-se-activitibpmn:process_file2>
+ 			<petals-se-activitibpmn:version2>1</petals-se-activitibpmn:version2>
 
 		</jbi:provides>
 	</jbi:services>

src/main/java/org/ow2/petals/activitibpmn/ActivitiSuManager.java
@@ -269,8 +269,12 @@ public class ActivitiSuManager extends AbstractServiceUnitManager {
                     bpmnModels.put(processFileName,
                             this.readBpmnModel(processFileName, versionStr, tenantId, categoryId, suRootPath));
                     nbProcesses++;
-                } else {
+                } else if ((processFileName != null && versionStr == null)
+                        || (processFileName == null && versionStr != null)) {
                     throw new IncoherentProcessDefinitionDeclarationException(processFileName, versionStr);
+                } else {
+                    // Here, processFileName == null and versionStr == null, and at least one process definition was
+                    // previously read, so we have nothing to do, we will exit the loop
                 }
             } while (processFileName != null && versionStr != null);
         } else if (processFileName != null && versionStr != null) {

src/test/java/org/ow2/petals/activitibpmn/ActivitiSuManagerTest.java
@@ -761,4 +761,81 @@ public class ActivitiSuManagerTest extends AbstractTest {
             assertTrue(error.getThrown() instanceof IncoherentProcessDefinitionDeclarationException);
         }
     }
+
+    /**
+     * <p>
+     * Try do deploy an valid SU containing multiple process declarations.
+     * </p>
+     * <p>
+     * Expected results: No error occurs
+     * </p>
+     */
+    @Test
+    public void deploy_MultipleProcessDefinition() throws SecurityException, IllegalArgumentException,
+            CDKJBIDescriptorException, NoSuchFieldException, IllegalAccessException, IOException, JBIException,
+            URISyntaxException {
+
+        final String suName = "MultipleProcessDefintion";
+        COMPONENT_UNDER_TEST.deployService(suName, new ServiceConfigurationFactory() {
+            @Override
+            public ServiceConfiguration create() {
+
+                final URL wsdlUrl = Thread.currentThread().getContextClassLoader()
+                        .getResource("su/valid/vacationRequest.wsdl");
+                assertNotNull("WSDl not found", wsdlUrl);
+                final ServiceConfiguration serviceConfiguration = new ServiceConfiguration(AbstractComponentTest.class
+                        .getSimpleName(), AbstractComponentTest.VACATION_INTERFACE,
+                        AbstractComponentTest.VACATION_SERVICE, AbstractComponentTest.VACATION_ENDPOINT,
+                        ServiceType.PROVIDE, wsdlUrl);
+
+                final URL demanderCongesResponseXslUrl = Thread.currentThread().getContextClassLoader()
+                        .getResource("su/valid/demanderCongesResponse.xsl");
+                assertNotNull("Output XSL 'demanderCongesResponse.xsl' not found", demanderCongesResponseXslUrl);
+                serviceConfiguration.addResource(demanderCongesResponseXslUrl);
+
+                final URL validerDemandeResponseXslUrl = Thread.currentThread().getContextClassLoader()
+                        .getResource("su/valid/validerDemandeResponse.xsl");
+                assertNotNull("Output XSL 'validerDemandeResponse.xsl' not found", validerDemandeResponseXslUrl);
+                serviceConfiguration.addResource(validerDemandeResponseXslUrl);
+
+                final URL ajusterDemandeResponseXslUrl = Thread.currentThread().getContextClassLoader()
+                        .getResource("su/valid/ajusterDemandeResponse.xsl");
+                assertNotNull("Output XSL 'ajusterDemandeResponse.xsl' not found", ajusterDemandeResponseXslUrl);
+                serviceConfiguration.addResource(ajusterDemandeResponseXslUrl);
+
+                final URL numeroDemandeInconnuXslUrl = Thread.currentThread().getContextClassLoader()
+                        .getResource("su/valid/numeroDemandeInconnu.xsl");
+                assertNotNull("Output XSL 'numeroDemandeInconnu.xsl' not found", numeroDemandeInconnuXslUrl);
+                serviceConfiguration.addResource(numeroDemandeInconnuXslUrl);
+
+                final URL demandeDejaValideeXslUrl = Thread.currentThread().getContextClassLoader()
+                        .getResource("su/valid/demandeDejaValidee.xsl");
+                assertNotNull("Output XSL 'demandeDejaValidee.xsl' not found", demandeDejaValideeXslUrl);
+                serviceConfiguration.addResource(demandeDejaValideeXslUrl);
+
+                final URL bpmnUrl = Thread.currentThread().getContextClassLoader()
+                        .getResource("su/valid/vacationRequest.bpmn20.xml");
+                assertNotNull("BPMN file not found", bpmnUrl);
+                serviceConfiguration.addResource(bpmnUrl);
+
+                // First process definition
+                serviceConfiguration.setParameter(
+                        "{http://petals.ow2.org/components/petals-se-activitibpmn/version-1.0}process_file1",
+                        "vacationRequest.bpmn20.xml");
+                serviceConfiguration.setParameter(
+                        "{http://petals.ow2.org/components/petals-se-activitibpmn/version-1.0}version1", "1");
+
+                // 2nd process definition
+                serviceConfiguration.setParameter(
+                        "{http://petals.ow2.org/components/petals-se-activitibpmn/version-1.0}process_file2",
+                        "vacationRequest.bpmn20.xml");
+                serviceConfiguration.setParameter(
+                        "{http://petals.ow2.org/components/petals-se-activitibpmn/version-1.0}version2", "1");
+
+                return serviceConfiguration;
+            }
+        });
+
+        assertNotNull(COMPONENT_UNDER_TEST.getServiceConfiguration(suName));
+    }
 }