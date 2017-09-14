4480981d5922c0a8d205532cc538d9307044f423
cdeneux <christophe.deneux@gmail.com>
2017-09-14 12:23:03 +0200

    Fix previous merge

--- src/main/java/org/ow2/petals/flowable/outgoing/cxf/transport/PetalsConduit.java
+++ src/main/java/org/ow2/petals/flowable/outgoing/cxf/transport/PetalsConduit.java
@@ -41,6 +41,7 @@ import org.apache.cxf.ws.addressing.EndpointReferenceType;
 import org.flowable.engine.impl.context.Context;
 import org.ow2.easywsdl.wsdl.api.abstractItf.AbsItfOperation.MEPPatternConstants;
 import org.ow2.petals.commons.log.FlowAttributes;
+import org.ow2.petals.component.framework.api.exception.PEtALSCDKException;
 import org.ow2.petals.component.framework.listener.AbstractListener;
 import org.ow2.petals.component.framework.util.SourceUtil;
 import org.w3c.dom.Document;
