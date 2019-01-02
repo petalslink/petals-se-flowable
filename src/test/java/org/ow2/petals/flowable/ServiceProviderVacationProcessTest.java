a2649706cba5e47d781fe0dcd02c9559bee7a72f
cdeneux <christophe.deneux@gmail.com>
2019-01-02 16:30:53 +0100

    PETALSDISTRIB-398: Update copyrights according to 2019

--- src/test/java/org/ow2/petals/flowable/ServiceProviderVacationProcessTest.java
+++ src/test/java/org/ow2/petals/flowable/ServiceProviderVacationProcessTest.java
@@ -1,6 +1,6 @@
 /**
- * Copyright (c) 2014-2018 Linagora
- *
+ * Copyright (c) 2014-2019 Linagora
+ * 
  * This program/library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 2.1 of the License, or (at your
